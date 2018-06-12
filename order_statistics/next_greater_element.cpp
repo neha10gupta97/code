bc9f5bbd34d959f3e81c72be73fd9f78d4f426e4
Neha Gupta <neha10gupta97@gmail.com>
2018-06-12 11:35:33 +0530

    find the next greater element for each wach element in array

---
+++ order_statistics/next_greater_element.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void nextgreaterelement(vector<int>arr,int n){
+  stack<int> st;
+  vector<int>ans(n);
+  int i;
+  for(i=n-1;i>=0;i--){
+    if(i==n-1){
+      ans[i]=-1;
+    }
+    else{
+      if(!st.empty() && st.top()>arr[i]){
+        ans[i]=st.top();
+      }
+      else{
+        int f = 0;
+        while(!st.empty()){
+          if(st.top()<=arr[i])
+            st.pop();
+          else{
+            ans[i]=st.top();
+            f=1;
+            break;
+          }
+        }
+        if(!f){
+          ans[i]=-1;
+        }
+      }
+    }
+    st.push(arr[i]);
+  }
+  cout<<"Given array: ";
+  for(i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+  }
+  cout<<endl;
+  cout<<"Next greater element array: ";
+  for(i=0;i<n;i++){
+    cout<<ans[i]<<" ";
+  }
+  cout<<endl;
+}
+
+main(){
+  int i,n;
+  cin>>n;
+  vector<int>arr(n);
+  for(i=0;i<n;i++)
+    cin>>arr[i];
+  nextgreaterelement(arr,n);
+}
