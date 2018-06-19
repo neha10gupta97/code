baee11a5b15ef99096811ef1965a132a81e8804e
Neha Gupta <neha10gupta97@gmail.com>
2018-06-19 15:07:18 +0530

    find total no of strictly inc subarrays

---
+++ order_statistics/find_no_of_strictly_inc_subarrays.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//inc array of len l gives l*(l-1)/2 subarrays
+
+int maxIncSubarray(vector<int>arr,int n){
+  int i=1;
+  int count = 0,l=0;
+  while(i<n){
+    while(i<n && arr[i]>arr[i-1])
+      i++;
+    count += (i-l)*(i-l-1)/2;
+    l=i;
+    if(i<n&&arr[i]<arr[i-1])
+      i++;
+  }
+  return count;
+}
+
+main(){
+  int i,n;
+  cin>>n;
+  vector<int>arr(n);
+  for(i=0;i<n;i++)
+    cin>>arr[i];
+  int x=maxIncSubarray(arr,n);
+  cout<<"Max no of structly inc subarray: "<<x<<endl;
+}
