5888de189eda104e73348421195b29eb63014a98
Neha Gupta <neha10gupta97@gmail.com>
2018-06-28 18:35:23 +0530

    peak element in array correct

--- searching_and_sorting/peak_element.cpp
+++ searching_and_sorting/peak_element.cpp
@@ -1,18 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int peak(vector<int>arr,int l,int h){
-  if(l==h)
-    return l;
+int peak(vector<int>arr,int l,int h,int n){
   int m = (l+h)/2;
-  if(m>l && m<h && arr[m]>arr[m-1] && arr[m]>arr[m+1])
+  if((m==0 || arr[m]>arr[m-1]) && (m==n-1 || arr[m]>arr[m+1]))
     return m;
   else if(m>l && arr[m]<arr[m-1]){
-    return peak(arr,l,m-1);
+    return peak(arr,l,m-1,n);
   }
-  else if(m<h && arr[m]<arr[m+1]){
-    return peak(arr,m+1,h);
-  }
+  return peak(arr,m+1,h,n);
 }
 
 main(){
@@ -21,7 +17,7 @@ main(){
   vector<int>arr(n);
   for(i=0;i<n;i++)
     cin>>arr[i];
-  int peake = peak(arr,0,n-1);
+  int peake = peak(arr,0,n-1,n);
 
   cout<<"Peak element: "<<arr[peake]<<endl;
 }
