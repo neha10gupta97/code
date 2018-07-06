201e20590ccf1c7c1428b7b1c58b69a5a61eabbe
Neha Gupta <neha10gupta97@gmail.com>
2018-07-06 14:44:14 +0530

    insert at n in ll

--- singly_linked_list/insert_in_ll.cpp
+++ singly_linked_list/insert_in_ll.cpp
@@ -6,19 +6,53 @@ struct node{
   node* next;
 };
 
-class{
-  node head;
-  public:
-    node(int d){
-      head->data = d;
-      head->next =NULL;
+void insertatn(node** head,int n,int ele){
+  node* temp = *head;
+  int len=1;
+  if(len==n){
+    node* cur = new node();
+    cur->data = ele;
+    cur->next=(*head);
+    *head = cur;
+    return;
+  }
+  while(temp!=NULL){
+    len++;
+    if(len==n){
+      node* cur = new node();
+      cur->data = ele;
+      cur->next = temp->next;
+      temp->next = cur;
+      return;
     }
+    temp = temp->next;
+  }
+  cout<<n<<" > size of linked list\n";
+}
 
-    insert(int x){
+void print(node* head){
+  node* temp = head;
+  while(temp!=NULL){
+    cout<<temp->data;
+    temp=temp->next;
+  }
+  cout<<endl;
+}
 
-    }
+void push(node** head,int ele){
+  node* cur = new node();
+  cur->data = ele;
+  cur->next=(*head);
+  *head = cur;
 }
 
 main(){
-  int n
+  node* head = NULL;
+  push(&head, 1);
+  push(&head, 7);
+  insertatn(&head,1,8);
+  insertatn(&head,5,5);
+  insertatn(&head,2,11);
+
+  print(head);
 }
