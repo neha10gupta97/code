068c1259365287ccd95fb89aa6dd19fbc6cc2918
Neha Gupta <neha10gupta97@gmail.com>
2018-07-06 08:53:32 +0530

    sibly linked list intialised

---
+++ singly_linked_list/insert_in_ll.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node{
+  int data;
+  node* next;
+};
+
+class{
+  node head;
+  public:
+    node(int d){
+      head->data = d;
+      head->next =NULL;
+    }
+
+    insert(int x){
+
+    }
+}
+
+main(){
+  int n
+}
